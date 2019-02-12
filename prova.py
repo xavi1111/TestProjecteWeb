66f59b4817ffee828aed703f053a4d6cbb819db2
Xavi <xavierllubes11@gmail.com>
2019-02-12 10:56:35 +0100

    Versio final dia 1

--- prova.py
+++ prova.py
@@ -1,25 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf8 :
 
 from urllib.request import urlopen
+import bs4
 
 class ClientWeb(object):
-    """client web per la web de la EPS"""
+    """CLient web per la web de la EPS"""
     def __init__(self):
         super(ClientWeb, self).__init__()
-        pass
 
     def descarregar_html(self):
         f = urlopen("http://www.eps.udl.cat/ca/")
-        html = f.read
-        f.close
+        html = f.read()
+        f.close()
         return html
 
-    def run(self):
-        #descarregar-me html
-        html=self.descarregar_html()
-        #buscar activitats
-        #imprimir resultat
-        print(html)
+    def buscar_activitats(self, html):
+        arbre = bs4.BeautifulSoup(html, features="lxml")
+        activitats = arbre.find_all("div","featured-links-item")
+        return activitats
 
-if __name__== "__main__":
-    c=ClientWeb()
+    def run(self):
+        # descarregar-me html
+        html = self.descarregar_html()
+        # buscar activitats
+        activitats = self.buscar_activitats(html)
+        # imprimir resultat
+        print(activitats)
+
+if __name__ == "__main__":
+    c = ClientWeb()
     c.run()
